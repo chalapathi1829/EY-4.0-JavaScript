ccb8bebb7e123a5ce123e36df5ae80a740057647
chalapathi1829 <chalapathinaidunallani@gmail.com>
2025-09-22 16:20:17 +0530

    predefine fun

---
+++ predefine fun.js
@@ -0,0 +1,17 @@
+var a= Number(prompt("Enter a number"));
+var b= Number(prompt("Enter b number"));
+
+document.write(`The value of a is ${a} <br> The value of b is ${b} <br>`);
+
+function add(){
+    document.write(alert(a+b));
+}
+function sub(){
+    document.write(alert(a-b));
+}
+function mul(){
+    document.write(alert(a*b));
+}
+function div(){
+    document.write(alert(a/b));
+}
