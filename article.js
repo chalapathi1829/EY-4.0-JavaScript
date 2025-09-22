572d18a3a53966325fafa10820660449d814a18d
chalapathi1829 <chalapathinaidunallani@gmail.com>
2025-09-22 15:19:51 +0530

    article

---
+++ article.js
@@ -0,0 +1,12 @@
+function box1(btn) {
+    btn.closest(".box1").style.backgroundColor = "lightblue";
+}
+function box2(btn) {
+    btn.closest(".box2").style.backgroundColor = "lightgreen";
+}
+function box3(btn) {
+    btn.closest(".box3").style.backgroundColor = "lightyellow";
+}
+function box4(btn) {
+    btn.closest(".box4").style.backgroundColor = "lightpink";
+}
