f075ba81d41acbd2b198f4634ce44c8546320ff7
chalapathi1829 <chalapathinaidunallani@gmail.com>
2025-09-22 14:13:10 +0530

    change3

--- function.js
+++ function.js
@@ -9,10 +9,28 @@
 // add();
 
 //ES6
-const add=()=>{
-    var a=85;
-    var b=15;
-    console.log(a+b);
-    document.write(a+b);
+// const add=()=>{
+//     var a=85;
+//     var b=15;
+//     console.log(a+b);
+//     document.write(a+b);
+// }
+// add();
+
+//ES7
+// const add=(a,b)=> console.log(a+b);
+// add(12,34);
+
+//b is not defined
+//hoisting => is it to decleare a variable before using it  (ES6)
+// console.log(b);
+// b=45;
+
+let name="john";
+
+function demo(){
+    let name="peter";
+    document.write(`Hello!, My name is ${name}`);
 }
-add();
+document.write(`Hello!, My name is ${name}<br>`);
+demo();
