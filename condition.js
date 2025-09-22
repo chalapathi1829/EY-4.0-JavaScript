f59c742a9680ee09373da24f3b9756eb0dd81e50
chalapathi1829 <chalapathinaidunallani@gmail.com>
2025-09-22 16:49:36 +0530

    if nested-if for

---
+++ condition.js
@@ -0,0 +1,26 @@
+// Conditional Statements and Loops in JavaScript
+
+// Example of if-else statement
+a=18
+if(a<18){
+    document.write("You are a minor.");
+} else{
+    document.write("You are an adult.");
+}
+
+// example of nested if-else statement
+age=60
+if(age<18){
+    document.write("<br>You are a minor.");
+}
+else if(age>=18 && age<60){
+    document.write("<br>You are an adult.");
+}
+else{
+    document.write("<br>You are a senior citizen.");
+}
+// Example of for loop
+for(i=0; i<5; i++){
+    // document.write("<br>" + i);
+    document.write("<br>"+i,"\n");
+}
